d88fd3032ab0c89b12f25ddc48d72353f2af6a94
slifszyc <sacha.lifszyc@gmail.com>
2016-08-23 17:21:44 -0500

    Agregar OffCanvas menu

--- js/app.js
+++ js/app.js
@@ -3,3 +3,13 @@ $(document).foundation()
 $('[data-toggle-dia]').click(function () {
   $('#lineup-tabs').foundation('selectTab', $(this).data('toggleDia'))
 });
+
+var $offCanvas = $('#offCanvas');
+var $sticky = $('#sticky');
+
+$offCanvas.find('li').click(() => {
+  $offCanvas.foundation('close');
+  setTimeout(() => {
+    $sticky.css('left', '0px');
+  }, 300)
+})
